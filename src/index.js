a9fdb0f11b99ef574ce85ece1dcc51ea526b1d24
Matt Brennan <mattyb1000@gmail.com>
2015-04-11 12:25:16 +0100

    giant refactor

--- src/index.js
+++ src/index.js
@@ -7,95 +7,96 @@ var {
 	concatMap, find
 } = require("data.array");
 
-class Saito {
-	constructor(spec) {
-		this.tasks = spec.call(this);
+function resolveTask(tasks, name) {
+	var task;
+
+	if(name in tasks) {
+		return {name};
 	}
 
-	task(name) {
-		var results = {};
-		var order = toposort(this.edges(name)).reverse();
-		for(let t of iter(order.length ? order : [name])) {
-			let spec = this.resolveTask(t);
-			let task = this.getTask(spec);
-			let args = this.getDeps(task, spec).map(d => {
-				return results[this.resolveTask(d).name];
-			});
-
-			results[spec.name] = task(...args);
-		}
-
-		return results[this.resolveTask(name).name];
+	task = find(pattern.match([name]), Object.keys(tasks));
+	if(task.isJust) {
+		return {name: task.get()};
 	}
 
-	dep(...deps) {
-		var fn = deps.pop();
-		fn.deps = deps;
-		return fn;
+	task = pattern.match(Object.keys(tasks), name);
+	if(task) {
+		return task;
 	}
 
-	resolveTask(name) {
-		var task;
+	throw new ReferenceError(`No such task ${name}`);
+}
 
-		if(name in this.tasks) {
-			return {name};
-		}
+function getTask(tasks, {pattern, name}) {
+	return tasks[pattern || name];
+}
 
-		task = find(pattern.match([name]), Object.keys(this.tasks));
-		if(task.isJust) {
-			return {name: task.get()};
-		}
+function getDeps(task, spec = {}) {
+	var deps = task.deps || [];
 
-		task = pattern.match(Object.keys(this.tasks), name);
-		if(task) {
-			return task;
-		}
-
-		throw new ReferenceError(`No such task ${name}`);
-	}
-
-	getTask({pattern, name}) {
-		return this.tasks[pattern || name];
-	}
-
-	getDeps(task, spec = {}) {
-		var deps = task.deps || [];
-
-		if(spec.stem) {
-			return deps.map(d => pattern.interpolate(d, spec.stem));
-		} else {
-			return deps;
-		}
-	}
-
-	edges(start) {
-		if(start) {
-			return this.findEdges(start);
-		} else {
-			return concatMap(name => {
-				return this.getDeps(this.tasks[name]).map(dep => {
-					return [name, dep];
-				});
-			}, Object.keys(this.tasks));
-		}
-	}
-
-	findEdges(name, stack = []) {
-		if(stack.indexOf(name) !== -1) {
-			throw new Error(`Circular dependency: ${stack.concat(name).join(" → ")}`);
-		}
-
-		var spec = this.resolveTask(name);
-		var task = this.getTask(spec);
-		var deps = this.getDeps(task, spec);
-
-		return concatMap(
-			x => this.findEdges(x, stack.concat(name)),
-			deps
-		).concat(deps.map(dep => [name, dep]));
+	if(spec.stem) {
+		return deps.map(d => pattern.interpolate(d, spec.stem));
+	} else {
+		return deps;
 	}
 }
 
+function findEdges(tasks, name, stack = []) {
+	if(stack.indexOf(name) !== -1) {
+		throw new Error(`Circular dependency: ${stack.concat(name).join(" → ")}`);
+	}
+
+	var spec = resolveTask(tasks, name);
+	var task = getTask(tasks, spec);
+	var deps = getDeps(task, spec);
+
+	return concatMap(
+		x => findEdges(tasks, x, stack.concat(name)),
+		deps
+	).concat(deps.map(dep => [name, dep]));
+}
+
+function edges(tasks, start) {
+	if(start) {
+		return findEdges(tasks, start);
+	} else {
+		return concatMap(name => {
+			return getDeps(tasks[name]).map(dep => {
+				return [name, dep];
+			});
+		}, Object.keys(tasks));
+	}
+}
+
+function task(tasks, name) {
+	var results = {};
+	var order = toposort(edges(tasks, name)).reverse();
+
+	for(let t of iter(order.length ? order : [name])) {
+		let spec = resolveTask(tasks, t);
+		let taskFn = getTask(tasks, spec);
+		let args = getDeps(taskFn, spec).map(d => {
+			return results[resolveTask(tasks, d).name];
+		});
+
+		results[spec.name] = taskFn(...args);
+	}
+
+	return results[resolveTask(tasks, name).name];
+}
+
 module.exports = function factory(spec) {
-	return new Saito(spec);
+	var tasks = spec.call({
+		dep(...deps) {
+			var fn = deps.pop();
+			fn.deps = deps;
+			return fn;
+		}
+	});
+
+	return {
+		task: (name) => task(tasks, name),
+		resolveTask: (name) => resolveTask(tasks, name),
+		edges: (start) => edges(tasks, start)
+	};
 };
